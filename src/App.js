700a3287fbd000fd1db80b516e00e86e579daa0b
Marlon Peña <marlon231098@hotmail.com>
2023-02-09 12:42:06 -0400

    Implementing react-route structure

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
 import React, { useState } from "react";
-import QuestionCard from "./components/QuestionCard";
-import FinalResults from "./components/FinalResults";
-import questionBank from "./data/questions.json";
-
+import Home from "./pages/Home";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Quiz from "./pages/Quiz";
 
 function App() {
-  const [finalResults, setFinalResults] = useState(false);
-  const [score, setScore] = useState(0);
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const questions = questionBank;
-
-  const optionClicked = (isCorrect) => {
-    if (isCorrect) setScore(score + 1);
-
-    currentQuestion + 1 < questions.length
-      ? setCurrentQuestion(currentQuestion + 1)
-      : setFinalResults(true);
-  };
-
-  const restartGame = () => {
-    setScore(0);
-    setCurrentQuestion(0);
-    setFinalResults(false);
-  };
-
   return (
     <div className="App">
-      <h1>Header</h1>
-
-      <h2>Current Score: {score}</h2>
-      {finalResults ? (
-        <FinalResults
-          score={score}
-          questions={questions}
-          restartGame={restartGame}
-        />
-      ) : (
-        <QuestionCard
-          currentQuestion={currentQuestion}
-          questions={questions}
-          optionClicked={optionClicked}
-        />
-      )}
+      {
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
+      }
     </div>
   );
 }
---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,47 @@
+import React, { Component, useState } from "react";
+import QuestionCard from "../components/QuestionCard";
+import FinalResults from "../components/FinalResults";
+import questionBank from "../data/questions.json";
+
+function Quiz() {
+  const [finalResults, setFinalResults] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const questions = questionBank;
+
+  const optionClicked = (isCorrect) => {
+    if (isCorrect) setScore(score + 1);
+
+    currentQuestion + 1 < questions.length
+      ? setCurrentQuestion(currentQuestion + 1)
+      : setFinalResults(true);
+  };
+
+  const restartGame = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setFinalResults(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>Header</h1>
+      <h2>Current Score: {score}</h2>
+      {finalResults ? (
+        <FinalResults
+          score={score}
+          questions={questions}
+          restartGame={restartGame}
+        />
+      ) : (
+        <QuestionCard
+          currentQuestion={currentQuestion}
+          questions={questions}
+          optionClicked={optionClicked}
+        />
+      )}{" "}
+    </div>
+  );
+}
+
+export default Quiz;
